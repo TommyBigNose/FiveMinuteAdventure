8b4f21d5c4de90e628c868ebda596a7ae206ca33
Thomas L <thomaslachin89@gmail.com>
2024-04-29 21:35:30 -0500

    Basic setup for stats

---
+++ Source/FiveMinuteAdventure/FiveMinuteAdventure/Systems/Models/Stats.cs
@@ -0,0 +1,38 @@
+﻿namespace FiveMinuteAdventure.Systems.Models;
+
+public class Stats
+{
+    public int Level { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Speed { get; set; }
+
+    public Stats()
+    {
+        Level = 1;
+        Attack = 1;
+        Defense = 1;
+        Speed = 1;
+    }
+    
+    public Stats(Stats clone)
+    {
+        Level = clone.Level;
+        Attack = clone.Attack;
+        Defense = clone.Defense;
+        Speed = clone.Speed;
+    }
+
+    public Stats CompareDifferenceInStats(Stats comparedStats)
+    {
+        Stats stats = new()
+        {
+            Level = Level - comparedStats.Level,
+            Attack = Attack - comparedStats.Attack,
+            Defense = Defense - comparedStats.Defense,
+            Speed = Speed - comparedStats.Speed
+        };
+
+        return stats;
+    }
+}
---
+++ Source/FiveMinuteAdventure/FiveMinuteAdventureTests/Systems/Models/StatsTests.cs
@@ -0,0 +1,62 @@
+namespace FiveMinuteAdventureTests.Systems.Models;
+
+public class StatsTests
+{
+    private Stats _sut;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new Stats();
+    }
+
+    [Test]
+    public void Should_Clone_When_GivenAnotherModel()
+    {
+        // Arrange
+        Stats clonedStats = new Stats()
+        {
+            Level = 10,
+            Attack = 5,
+            Defense = 3,
+            Speed = 4
+        };
+        
+        // Act
+        var result = new Stats(clonedStats);
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Level == clonedStats.Level);
+            Assert.That(result.Attack == clonedStats.Attack);
+            Assert.That(result.Defense == clonedStats.Defense);
+            Assert.That(result.Speed == clonedStats.Speed);
+        });
+    }
+    
+    [Test]
+    public void Should_Compare_When_GivenAnotherModel()
+    {
+        // Arrange
+        Stats statsToCompare = new Stats()
+        {
+            Level = 2,
+            Attack = 3,
+            Defense = 4,
+            Speed = 5
+        };
+        
+        // Act
+        var result = _sut.CompareDifferenceInStats(statsToCompare);
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Level == _sut.Level - statsToCompare.Level);
+            Assert.That(result.Attack == _sut.Attack - statsToCompare.Attack);
+            Assert.That(result.Defense == _sut.Defense - statsToCompare.Defense);
+            Assert.That(result.Speed == _sut.Speed - statsToCompare.Speed);
+        });
+    }
+}
